0a7058943f303b91a8ebecd779556512af9eab39
Faraz-hussain102 <cfaraz407@gmail.com>
2025-10-04 07:42:57 -0700

    Updated splash and signup screens with assets

--- lib/sign_up_screen.dart
+++ lib/sign_up_screen.dart
@@ -1,6 +1,189 @@
-import 'package:appfusion/log_in_page.dart';
+
+
+// import 'package:flutter/material.dart';
+// import 'package:appfusion/log_in_page.dart';
+
+// class SignUpScreen extends StatelessWidget {
+//   const SignUpScreen({super.key});
+
+//   @override
+//   Widget build(BuildContext context) {
+//     return Scaffold(
+//       backgroundColor: Colors.white,
+//       body: Stack(
+//         children: [
+//           Positioned(
+//             top: 0,
+//             left: MediaQuery.of(context).size.width / 2 - 125,
+//             child: Image.asset('assets/logo3.png', height: 250, width: 250),
+//           ),
+//           Positioned(
+//             top: 150,
+//             left: 30,
+//             right: 30,
+//             child: Column(
+//               children: [
+//                 TextFormField(
+//                   decoration: InputDecoration(
+//                     hintText: 'Enter Your Phone Or Email',
+//                     prefixIcon: Icon(Icons.email_outlined),
+//                     prefixIconColor: Color(0xff747474),
+//                     hintStyle: TextStyle(
+//                       color: Colors.blueGrey,
+//                       fontWeight: FontWeight.bold,
+//                     ),
+//                     enabledBorder: OutlineInputBorder(
+//                       borderSide: BorderSide(color: Colors.black),
+//                       borderRadius: BorderRadius.circular(10),
+//                     ),
+//                     focusedBorder: OutlineInputBorder(
+//                       borderSide: BorderSide(color: Colors.black),
+//                       borderRadius: BorderRadius.circular(10),
+//                     ),
+//                   ),
+//                 ),
+//                 SizedBox(height: 20),
+//                 TextFormField(
+//                   obscureText: true,
+//                   decoration: InputDecoration(
+//                     hintText: 'Password',
+//                     prefixIcon: Icon(Icons.lock),
+//                     prefixIconColor: Color(0xff747474),
+//                     hintStyle: TextStyle(
+//                       color: Colors.blueGrey,
+//                       fontWeight: FontWeight.bold,
+//                     ),
+//                     enabledBorder: OutlineInputBorder(
+//                       borderSide: BorderSide(color: Colors.black),
+//                       borderRadius: BorderRadius.circular(10),
+//                     ),
+//                     focusedBorder: OutlineInputBorder(
+//                       borderSide: BorderSide(color: Colors.black),
+//                       borderRadius: BorderRadius.circular(10),
+//                     ),
+//                   ),
+//                 ),
+//                 SizedBox(height: 20),
+//                 TextFormField(
+//                   obscureText: true,
+//                   decoration: InputDecoration(
+//                     hintText: 'Confirm Password',
+//                     prefixIcon: Icon(Icons.lock),
+//                     prefixIconColor: Color(0xff747474),
+//                     hintStyle: TextStyle(
+//                       color: Colors.blueGrey,
+//                       fontWeight: FontWeight.bold,
+//                     ),
+//                     enabledBorder: OutlineInputBorder(
+//                       borderSide: BorderSide(color: Colors.black),
+//                       borderRadius: BorderRadius.circular(10),
+//                     ),
+//                     focusedBorder: OutlineInputBorder(
+//                       borderSide: BorderSide(color: Colors.black),
+//                       borderRadius: BorderRadius.circular(10),
+//                     ),
+//                   ),
+//                 ),
+//                 SizedBox(height: 10),
+//                 ElevatedButton(
+//                   onPressed: () {
+//                     Navigator.push(
+//                       context,
+//                       MaterialPageRoute(builder: (context) => LogInPage()),
+//                     );
+//                   },
+//                   style: ElevatedButton.styleFrom(
+//                     backgroundColor: Color(0xff2750E1),
+//                     padding: EdgeInsets.symmetric(horizontal: 120, vertical: 18),
+//                     shape: RoundedRectangleBorder(
+//                       borderRadius: BorderRadius.circular(10),
+//                     ),
+//                   ),
+//                   child: Text(
+//                     'Sign Up',
+//                     style: TextStyle(
+//                       color: Colors.white,
+//                       fontFamily: 'Roboto',
+//                       fontWeight: FontWeight.bold,
+//                       fontSize: 16,
+//                     ),
+//                   ),
+//                 ),
+//                 SizedBox(height: 60),
+//                 Container(
+//                   height: 60,
+//                   width: double.infinity,
+//                   decoration: BoxDecoration(
+//                     color: Colors.white,
+//                     border: Border.all(color: Colors.black),
+//                     borderRadius: BorderRadius.circular(10),
+//                   ),
+//                   child: Row(
+//                     mainAxisAlignment: MainAxisAlignment.center,
+//                     children: [Image.asset('assets/logo4.png')],
+//                   ),
+//                 ),
+//                 SizedBox(height: 20),
+//                 Container(
+//                   height: 60,
+//                   width: double.infinity,
+//                   decoration: BoxDecoration(
+//                     color: Colors.white,
+//                     border: Border.all(color: Colors.black),
+//                     borderRadius: BorderRadius.circular(10),
+//                   ),
+//                   child: Row(
+//                     mainAxisAlignment: MainAxisAlignment.center,
+//                     children: [Image.asset('assets/logo5.png')],
+//                   ),
+//                 ),
+//                 SizedBox(height: 15),
+//                 RichText(
+//                   text: TextSpan(
+//                     text: 'Already have an account? ',
+//                     style: TextStyle(
+//                       color: Colors.black,
+//                       fontSize: 16,
+//                       fontWeight: FontWeight.bold,
+//                     ),
+//                     children: <TextSpan>[
+//                       TextSpan(
+//                         text: 'Login',
+//                         style: TextStyle(fontSize: 16, color: Colors.blue),
+//                       ),
+//                     ],
+//                   ),
+//                 ),
+//               ],
+//             ),
+//           ),
+//         ],
+//       ),
+//     );
+//   }
+// }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 import 'package:flutter/material.dart';
+import 'package:appfusion/log_in_page.dart';
 
 class SignUpScreen extends StatelessWidget {
   const SignUpScreen({super.key});
@@ -14,79 +197,87 @@ class SignUpScreen extends StatelessWidget {
           Positioned(
             top: 0,
             left: MediaQuery.of(context).size.width / 2 - 125,
-            child: Image.asset('assets/logo3.png', height: 250, width: 250),
+            child: Image.asset(
+              'assets/logo3.png', // optimized image
+              height: 250,
+              width: 250,
+            ),
           ),
-
-          // TextFormFields and Button
           Positioned(
             top: 150,
             left: 30,
             right: 30,
             child: Column(
               children: [
+                // Email / Phone
                 TextFormField(
                   decoration: InputDecoration(
                     hintText: 'Enter Your Phone Or Email',
-                    prefixIcon: Icon(Icons.email_outlined),
-                    prefixIconColor: Color(0xff747474),
-                    hintStyle: TextStyle(
+                    prefixIcon: const Icon(Icons.email_outlined),
+                    prefixIconColor: const Color(0xff747474),
+                    hintStyle: const TextStyle(
                       color: Colors.blueGrey,
                       fontWeight: FontWeight.bold,
                     ),
                     enabledBorder: OutlineInputBorder(
-                      borderSide: BorderSide(color: Colors.black),
+                      borderSide: const BorderSide(color: Colors.black),
                       borderRadius: BorderRadius.circular(10),
                     ),
                     focusedBorder: OutlineInputBorder(
-                      borderSide: BorderSide(color: Colors.black),
+                      borderSide: const BorderSide(color: Colors.black),
                       borderRadius: BorderRadius.circular(10),
                     ),
                   ),
                 ),
-                SizedBox(height: 20),
+                const SizedBox(height: 20),
+
+                // Password
                 TextFormField(
                   obscureText: true,
                   decoration: InputDecoration(
                     hintText: 'Password',
-                    prefixIcon: Icon(Icons.lock),
-                    prefixIconColor: Color(0xff747474),
-                    hintStyle: TextStyle(
+                    prefixIcon: const Icon(Icons.lock),
+                    prefixIconColor: const Color(0xff747474),
+                    hintStyle: const TextStyle(
                       color: Colors.blueGrey,
                       fontWeight: FontWeight.bold,
                     ),
                     enabledBorder: OutlineInputBorder(
-                      borderSide: BorderSide(color: Colors.black),
+                      borderSide: const BorderSide(color: Colors.black),
                       borderRadius: BorderRadius.circular(10),
                     ),
                     focusedBorder: OutlineInputBorder(
-                      borderSide: BorderSide(color: Colors.black),
+                      borderSide: const BorderSide(color: Colors.black),
                       borderRadius: BorderRadius.circular(10),
                     ),
                   ),
                 ),
-                SizedBox(height: 20),
+                const SizedBox(height: 20),
+
+                // Confirm Password
                 TextFormField(
                   obscureText: true,
                   decoration: InputDecoration(
                     hintText: 'Confirm Password',
-                    prefixIcon: Icon(Icons.lock),
-                    prefixIconColor: Color(0xff747474),
-                    hintStyle: TextStyle(
+                    prefixIcon: const Icon(Icons.lock),
+                    prefixIconColor: const Color(0xff747474),
+                    hintStyle: const TextStyle(
                       color: Colors.blueGrey,
                       fontWeight: FontWeight.bold,
                     ),
                     enabledBorder: OutlineInputBorder(
-                      borderSide: BorderSide(color: Colors.black),
+                      borderSide: const BorderSide(color: Colors.black),
                       borderRadius: BorderRadius.circular(10),
                     ),
                     focusedBorder: OutlineInputBorder(
-                      borderSide: BorderSide(color: Colors.black),
+                      borderSide: const BorderSide(color: Colors.black),
                       borderRadius: BorderRadius.circular(10),
                     ),
                   ),
                 ),
+                const SizedBox(height: 10),
 
-                SizedBox(height: 10),
+                // Sign Up Button
                 ElevatedButton(
                   onPressed: () {
                     Navigator.push(
@@ -94,10 +285,9 @@ class SignUpScreen extends StatelessWidget {
                       MaterialPageRoute(builder: (context) => LogInPage()),
                     );
                   },
-
                   style: ElevatedButton.styleFrom(
-                    backgroundColor: Color(0xff2750E1),
-                    padding: EdgeInsets.symmetric(
+                    backgroundColor: const Color(0xff2750E1),
+                    padding: const EdgeInsets.symmetric(
                       horizontal: 120,
                       vertical: 18,
                     ),
@@ -105,16 +295,20 @@ class SignUpScreen extends StatelessWidget {
                       borderRadius: BorderRadius.circular(10),
                     ),
                   ),
-                  child: Text(
+                  child: const Text(
                     'Sign Up',
                     style: TextStyle(
-                      color: Colors.white,fontFamily: 'Roboto',
+                      color: Colors.white,
+                      fontFamily: 'Roboto',
                       fontWeight: FontWeight.bold,
                       fontSize: 16,
                     ),
                   ),
                 ),
-                SizedBox(height: 60),
+
+                const SizedBox(height: 60),
+
+                // Social Buttons
                 Container(
                   height: 60,
                   width: double.infinity,
@@ -125,10 +319,12 @@ class SignUpScreen extends StatelessWidget {
                   ),
                   child: Row(
                     mainAxisAlignment: MainAxisAlignment.center,
-                    children: [Image(image: AssetImage('assets/logo4.png'))],
+                    children: [
+                      Image.asset('assets/logo4.png'), // optimized image
+                    ],
                   ),
                 ),
-                SizedBox(height: 20),
+                const SizedBox(height: 20),
                 Container(
                   height: 60,
                   width: double.infinity,
@@ -139,22 +335,26 @@ class SignUpScreen extends StatelessWidget {
                   ),
                   child: Row(
                     mainAxisAlignment: MainAxisAlignment.center,
-                    children: [Image(image: AssetImage('assets/logo5.png'))],
+                    children: [
+                      Image.asset('assets/logo5.png'), // optimized image
+                    ],
                   ),
                 ),
-                SizedBox(height: 15),
+
+                const SizedBox(height: 15),
+
+                // Already have an account?
                 RichText(
-                  text: TextSpan(
-                    text: 'Already have an account?',
+                  text: const TextSpan(
+                    text: 'Already have an account? ',
                     style: TextStyle(
                       color: Colors.black,
                       fontSize: 16,
                       fontWeight: FontWeight.bold,
                     ),
-
                     children: <TextSpan>[
                       TextSpan(
-                        text: 'Login in',
+                        text: 'Login',
                         style: TextStyle(fontSize: 16, color: Colors.blue),
                       ),
                     ],
--- lib/splash_screen.dart
+++ lib/splash_screen.dart
@@ -1,6 +1,89 @@
+// import 'dart:async';
+// import 'package:appfusion/sign_up_screen.dart';
+// import 'package:flutter/material.dart';
+
+// class SplashScreen extends StatefulWidget {
+//   const SplashScreen({super.key});
+
+//   @override
+//   State<SplashScreen> createState() => _SplashScreenState();
+// }
+
+// class _SplashScreenState extends State<SplashScreen> {
+//   @override
+//   void initState() {
+//     super.initState();
+//     Timer(const Duration(seconds: 3), () {
+    
+//       Navigator.pushReplacement(
+//         context,
+//         MaterialPageRoute(builder: (context) => SignUpscreen()),
+//       );
+//     });
+//   }
+
+//   @override
+//   Widget build(BuildContext context) {
+//     return Scaffold(
+//       body: SafeArea(
+//         child: Stack(
+//           children: [
+//             Positioned(
+//               top: 0,
+//               left: 0,
+//               child: Container(
+//                 width: 340,
+//                 height: 260,
+//                 decoration: const BoxDecoration(
+//                   color: Color(0xff2750E1),
+//                   borderRadius: BorderRadius.only(
+//                     bottomRight: Radius.circular(300),
+//                   ),
+//                 ),
+//                 child: const Image(
+//                   image: AssetImage('assets/logo1.png'),
+//                 ),
+//               ),
+//             ),
+//             Positioned(
+//               bottom: 0,
+//               right: 0,
+//               child: Container(
+//                 height: 500,
+//                 width: 355,
+//                 decoration: const BoxDecoration(
+//                   color: Color(0xff2750E1),
+//                   borderRadius: BorderRadius.only(
+//                     topLeft: Radius.circular(300),
+//                   ),
+//                 ),
+//                 child: const Image(
+//                   image: AssetImage('assets/logo2.png'),
+//                 ),
+//               ),
+//             ),
+//           ],
+//         ),
+//       ),
+//     );
+//   }
+// }
+
+
+
+
+
+
+
+
+
+
+
+
+
 import 'dart:async';
-import 'package:appfusion/sign_up_screen.dart';
 import 'package:flutter/material.dart';
+import 'package:appfusion/sign_up_screen.dart';
 
 class SplashScreen extends StatefulWidget {
   const SplashScreen({super.key});
@@ -14,10 +97,9 @@ class _SplashScreenState extends State<SplashScreen> {
   void initState() {
     super.initState();
     Timer(const Duration(seconds: 3), () {
-    
       Navigator.pushReplacement(
         context,
-        MaterialPageRoute(builder: (context) => const SignUpScreen()), 
+        MaterialPageRoute(builder: (context) => const SignUpScreen()),
       );
     });
   }
@@ -40,8 +122,9 @@ class _SplashScreenState extends State<SplashScreen> {
                     bottomRight: Radius.circular(300),
                   ),
                 ),
-                child: const Image(
-                  image: AssetImage('assets/logo1.png'),
+                child: Image.asset(
+                  'assets/logo1.png', // make sure this is optimized & small
+                  fit: BoxFit.cover,
                 ),
               ),
             ),
@@ -49,16 +132,17 @@ class _SplashScreenState extends State<SplashScreen> {
               bottom: 0,
               right: 0,
               child: Container(
-                height: 500,
                 width: 355,
+                height: 500,
                 decoration: const BoxDecoration(
                   color: Color(0xff2750E1),
                   borderRadius: BorderRadius.only(
                     topLeft: Radius.circular(300),
                   ),
                 ),
-                child: const Image(
-                  image: AssetImage('assets/logo2.png'),
+                child: Image.asset(
+                  'assets/logo2.png', // optimize this image as well
+                  fit: BoxFit.cover,
                 ),
               ),
             ),
